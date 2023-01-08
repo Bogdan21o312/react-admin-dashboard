cb51d3083822a4121dfb7d57af3bac82d01f9ff7
Bogdan21o312 <lutskiv2010@gmail.com>
2023-01-08 02:18:18 +0200

    Updated config router

--- src/router/config.ts
+++ src/router/config.ts
@@ -1,3 +1,5 @@
-export const HOME_URL = "/home"
+export const HOME_URL = "/"
 
-export const ERROR_URL = "/error"
+export const ERROR404_URL = "/404"
+export const LOGIN_URL = "/login"
+export const REGISTER_URL = "/register"
