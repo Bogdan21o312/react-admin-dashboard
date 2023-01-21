6f09664a3dd9baf065ad9d3add80848e66c558a3
Bogdan21o312 <lutskiv2010@gmail.com>
2023-01-21 03:00:32 +0200

    Updated model IText

--- src/models/IText.ts
+++ src/models/IText.ts
@@ -1,23 +1,35 @@
 import {ComponentProps, ElementType, ReactNode} from "react";
 
-type ITextDarkProps<E extends ElementType = ElementType> = {
-    dark?: boolean
-    secondary?: never
+type ITextTitleProps<E extends ElementType = ElementType> = {
+    title?: boolean
+    text?: never
+    link?: never
+    center?: boolean
     as?: E;
 };
 
-type ITextSecondaryProps<E extends ElementType = ElementType> = {
-    dark?: never
-    secondary?: boolean
+type ITextTextProps<E extends ElementType = ElementType> = {
+    title?: never
+    text?: boolean
+    link?: never
+    center?: boolean
     as?: E;
 };
 
-type ITextOwnProps<E extends ElementType = ElementType> = ITextDarkProps | ITextSecondaryProps & {
+type ITextLinkProps<E extends ElementType = ElementType> = {
+    title?: never
+    text?: never
+    link?: boolean
+    center?: boolean
+    as?: E;
+};
+
+type ITextOwnProps<E extends ElementType = ElementType> = ITextTitleProps | ITextTextProps | ITextLinkProps & {
     as?: E;
     children: ReactNode;
 };
 
 
-// export const defaultElementText = "div";
+export const defaultElementText = "div";
 export type ITextProps<E extends ElementType> = ITextOwnProps<E>  &
     Omit<ComponentProps<E>, keyof ITextOwnProps>;
