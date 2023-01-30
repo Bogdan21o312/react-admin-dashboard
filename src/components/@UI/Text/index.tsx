95c8faff2da39a98a08c65e0b2a778338a85980b
Bogdan21o312 <lutskiv2010@gmail.com>
2023-01-30 03:59:47 +0200

    Update Text

--- src/components/@UI/Text/index.tsx
+++ src/components/@UI/Text/index.tsx
@@ -1,12 +1,32 @@
 import React, {ElementType} from 'react';
-import {ITextProps} from "../../../models/IText";
+import {defaultElementText, ITextProps} from "../../../models/IText";
 import classes from "./Text.module.scss";
 
-const defaultElementText = "div";
-const Index = <E extends ElementType = typeof defaultElementText>({as, secondary, dark, children, ...otherProps}: ITextProps<E>) => {
-    let classesName = classes.button
+const Index = <E extends ElementType = typeof defaultElementText>({as, title, text, children, center, link, ...otherProps}: ITextProps<E>) => {
     const TagName = as || defaultElementText;
-    return <TagName className={classesName} {...otherProps}>{children}</TagName>
+
+    const classesMain = classes.main
+    let classesName = [classesMain]
+    const classesTitle = classes.title
+    const classesText = classes.text
+    const classesLink = classes.link
+    const classesCenter = classes.center
+
+    if (title) {
+        classesName.push(classesTitle)
+    }
+    if (text) {
+        classesName.push(classesText)
+    }
+    if (link) {
+        classesName.push(classesLink)
+    }
+    if (center) {
+        classesName.push(classesCenter)
+    }
+
+
+    return <TagName className={classesName.join(' ')} {...otherProps}>{children}</TagName>
 };
 
 export default Index;
