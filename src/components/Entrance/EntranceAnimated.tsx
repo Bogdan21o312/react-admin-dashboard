cbeb900de52d00087dd3a88559a4c8b7705ee7aa
Bogdan21o312 <lutskiv2010@gmail.com>
2023-02-08 01:55:08 +0200

    Updated

--- src/components/Entrance/EntranceAnimated.tsx
+++ src/components/Entrance/EntranceAnimated.tsx
@@ -6,36 +6,24 @@ import {CSSTransition} from 'react-transition-group';
 interface EntranceAnimatedProps {
     children: ReactNode
     state: boolean
-    functiaaon: any
-    // styleClasses: any
 }
 
-const EntranceAnimated: FC<EntranceAnimatedProps> = ({state, children, functiaaon}) => {
-    const aa = functiaaon + 'ExitActive'
-    const bb = functiaaon + 'Exit'
-    const cc = functiaaon + 'EnterDone'
-    const dd = functiaaon + 'EnterActive'
-    console.log(classes.functiaaon + 'ExitActive')
-
-    const a = classes.aa
-    const b = `${classes + '.' + bb}`
-    const c = `${classes + '.' + cc}`
-    const d = `${classes + '.' + dd}`
-    // console.log(a)
-    // console.log(b)
+const EntranceAnimated: FC<EntranceAnimatedProps> = ({state, children}) => {
     return (
         <>
             <CSSTransition
                 in={state}
                 timeout={1250}
                 classNames={{
-                    enterActive: d,
-                    enterDone: c,
-                    exitActive: b,
-                    exitDone: a
+                    enterActive: classes.mainEnterActive,
+                    enterDone: classes.mainEnterDone,
+                    exitActive: classes.mainExit,
+                    exitDone: classes.mainExitActive
                 }}
             >
-                {children}
+                <div className={classes.mainEnter}>
+                    {children}
+                </div>
             </CSSTransition>
         </>
     );
